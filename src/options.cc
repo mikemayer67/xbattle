5f7cd9496b7aa3690ea38c6c0a322e8e93ae494a
mike <mikemayer67@vmwishes.com>
2015-10-30 11:21:41 -0400

    Move Option class to option struct inside the options.cc source file   (no need to expose Option class to the world)

--- src/options.cc
+++ src/options.cc
@@ -3,6 +3,18 @@
 
 using namespace std;
 
+struct option
+{
+  string         key;
+  StringList_t   args;
+  vector<option> subOptions;
+
+  option(      StringList_t::const_iterator &, 
+         const StringList_t::const_iterator &,
+         bool  inGroup=false );
+}
+
+
 void Options::init( const StringList_t &args )
 {
   //
@@ -15,5 +27,63 @@ void Options::init( const StringList_t &args )
   while(pos<end)
   {
     Option opt(pos,end);
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// option struct constructor
+////////////////////////////////////////////////////////////////////////////////
+
+option::option(       StringList_t::const_iterator &pos,
+                const StringList_t::const_iterator &end,
+                bool  inGroup )
+{
+  key = *(pos++);
+
+  if(key[0]!='-') 
+    throw runtime_error(string("Invalid option '")+key+"' (does not start with -)");
+
+  key.erase(0,1);
+
+  while(pos<end)
+  {
+    if(*pos=="{")
+    {
+      if(inGroup)
+        throw logic_error("CommandLine constructor failed to catch nested {} group");
+
+      if(!subOptions.empty())
+        throw runtime_error("Only one {} group allowed as argument for any given option");
+
+      StringList_t::const_iterator groupPos = ++pos;
+      StringList_t::const_iterator groupEnd =   pos;
+
+      while(groupEnd!=end && *groupEnd!="}") ++groupEnd;
+
+      if(groupEnd==end)
+        throw logic_error("ComandLine constructor failed to catch opening { without closing }");
+
+      while(groupPos!=groupEnd)
+      {
+        subOptions.push_back( Option(groupPos, groupEnd, true ) );
+      }
+
+      pos = groupEnd;
+      ++pos;
+    }
+    else if(*pos=="}")
+    {
+      throw logic_error("CommandLine constructor failed to catch closing } without an opening {");
+    }
+    else if(pos->at(0)=='-')
+    {
+      break;
+    }
+    else
+    {
+      string arg = *pos;
+      args.push_back(arg);
+      ++pos;
+    }
   }
 }
